77edc195c35a9e5cc33db726574b8ef8210c1bb0
este93 <petkovics93@yahoo.com>
2020-05-16 12:57:55 +0200

    Update findingContacts.js

--- findingContacts.js
+++ findingContacts.js
@@ -1,39 +1,3 @@
 var imported = document.createElement('script');
 imported.src = 'https://cdn.jsdelivr.net/npm/airtable@0.6.0/build/airtable.browser.min.js';
 document.head.appendChild(imported);
-
-imported.onload = function(){
-    var Airtable = require('airtable');
-    var base = new Airtable({ apiKey: 'keyKEDd4VpZwHDga6' }).base('apphQRB2KoFoHPxOG');
-    var contactId = '';
-    let button = document.createElement('button');
-    button.innerHTML = 'Go to contact';
-    button.setAttribute("style", "display: none;");
-    document.querySelectorAll('body')[0].appendChild(button)
-    var loadTable = function() {
-        var mail = document.querySelectorAll('.c-profile__details .email')[0].textContent;
-
-        base('Test').select({
-            filterByFormula: "{Email} = '" + mail + "'"
-        }).eachPage(function page(records, fetchNextPage) {
-            records.forEach(function(record) {
-                contactId = record.id;
-                if(record){
-                    button.setAttribute("style", 'display: block');
-                }
-            });
-
-            fetchNextPage();
-        }, function done(error) {
-            console.log(error);
-        });
-    };
-
-    button.addEventListener('click', function(){
-        var win = window.open("https://www.google.com/contacts/view/con_" + contactId, '_blank');
-        // var win = window.open("https://ecommerce.airportal.app/contacts/view/con_" + contactId, '_blank');
-        win.focus();
-    })
-
-    loadTable();
-};
